37038db87822b98424dcd36e833c11062b6fad23
Shawn Oakley <shawnoakley@gmail.com>
2014-01-19 19:16:32 -0500

    Parse function now re-configures output as two dataframes nested in an array.

--- lireg.py
+++ lireg.py
@@ -33,37 +33,60 @@ def parse(data):
 
 	row = random.randint(0, len(data)-1)
 
-	testing_data = data.pop(row)
+	row = data.pop(row)
+
+	templist = []
+	for item in row:
+		templist.append(item)
+
+	testing_data = []
+	testing_data.append(list(templist))
 	training_data = data
 
 	counter = 0
 
-	headers = ['Title', 'FullDescription', 'LocationRaw', 'LocationNormalized', 'ContractType', 'ContractTime', 'Company', 'Category', 'SalaryRaw', 'SalaryNormalized', 'SourceName']
+	headers = ['ID', 'Title', 'FullDescription', 'LocationRaw', 'LocationNormalized', 'ContractType', 'ContractTime', 'Company', 'Category', 'SalaryRaw', 'SalaryNormalized', 'SourceName']
 
 	while testing_size/original_size < 0.2 and training_size/original_size > 0.8:
 
-		print 'testing_size/original_size: '  + str(float(testing_size)/original_size)
-		print 'training_size/original_size: ' + str(float(training_size)/original_size) 
+		# print 'testing_size/original_size: '  + str(float(testing_size)/original_size)
+		# print 'training_size/original_size: ' + str(float(training_size)/original_size) 
 
 
 		counter += 1
 
 		row = random.randint(0, len(training_data) - 1)
 
-		print 'row: ' + str(row)
-		print 'count: ' + str(len(training_data))
+		# print 'row: ' + str(row)
+		# print 'count: ' + str(len(training_data))
 
-		testing_data.append(training_data.pop(row))
+		row = training_data.pop(row)
+		# print ''
+		# print row
+		# print ''
+		templist = []
+
+
+		for item in row:
+			templist.append(item)
+
+		testing_data.append(list(templist))
 
 		testing_size = float(len(testing_data))
 		training_size = float(len(training_data))
 
-		print 'testing_size: ' + str(len(testing_data))
-		print 'training_size: ' + str(len(training_data))
-		print ''
+		# print 'testing_size: ' + str(len(testing_data))
+		# print 'training_size: ' + str(len(training_data))
+		# print ''
 
-	return [pd.DataFame(training_data, columns=header), pd.DataFrame(testing_data, columns=headers)]
+	training_data.pop(0)
+	# testing_data.pop(0)
+
+	# print testing_data[0]
+	# print training_data[0]
+	return [pd.DataFrame(training_data, columns=headers), pd.DataFrame(testing_data, columns=headers)]
 
 split_data = parse(data)
 
-print split_data
+print split_data[0]
+print split_data[1]
